d13a921b6ebe2af55f3a7adad23036434347d8fa
Enrique Perez <jose.avalos@kavak.com>
2022-09-19 14:13:04 -0500

    :construction: (DriversListView): add drivers list view

---
+++ app/src/main/java/com/example/itijuanatest/ui/driverslist/DriversListFragment.kt
@@ -0,0 +1,14 @@
+package com.example.itijuanatest.ui.driverslist
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.itijuanatest.R
+
+class DriversListFragment : Fragment(R.layout.fragment_drivers_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
