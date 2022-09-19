8684956668b7a829e9715f3d4fa37107edf45f5f
Enrique Perez <jose.avalos@kavak.com>
2022-09-19 14:20:42 -0500

    :construction: (DriverDetail): add driver detail view

---
+++ app/src/main/java/com/example/itijuanatest/ui/driverdetail/DriverDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.example.itijuanatest.ui.driverdetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.itijuanatest.R
+
+class DriverDetailFragment : Fragment(R.layout.fragment_driver_detail) {
+
+}
